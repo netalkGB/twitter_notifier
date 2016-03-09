24d617d9f7df78e91089933659bc878c44874258
A <example>
2016-03-09 19:09:52 +0900

    改行

---
+++ vendor/bundle/ruby/2.2.0/bin/nokogiri.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\msys64\mingw64\bin\ruby.exe" "C:/Users/gb/IdeaProjects/twitter_notifier/vendor/bundle/ruby/2.2.0/bin/nokogiri" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\msys64\mingw64\bin\ruby.exe" "%~dpn0" %*
